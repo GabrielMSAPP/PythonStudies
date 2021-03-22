c56137a2517238e9eb44d00dc8fd4bd6fb08c0f3
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 19:55:17 -0300

    Create exercicio6.py

---
+++ ListaExercicios1/exercicio6.py
@@ -0,0 +1,4 @@
+'''Elaborar um programa que receba uma medida em pés e apresente o valor convertido em metros.'''
+
+pes = float(input('Informe uma media em pes:'))
+print(f'A sua media convertida em metros é: {pes * 0.3048}')
