1bd9a418b4c575a3738c5876a520cde6c76adeba
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 19:56:52 -0300

    Create exercicio20.py

---
+++ ListaExercicios1/exercicio20.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que calcule e exiba na tela o perímetro e área de uma circunferência. 
+Dados: area = Pi raio ** 2, perimetro = 2 Pi * raio.'''
+
+raio = float(input('Informe o raio de uma circunferencia: '))
+print(f' A area da circunferencia é: {3.14 * (raio*raio)}')
