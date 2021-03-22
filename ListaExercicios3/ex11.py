31972aa4905b4f9eed05c2ed2c57fc2bff4069d3
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 20:41:35 -0300

    Create ex11.py

---
+++ ListaExercicios3/ex11.py
@@ -0,0 +1,5 @@
+'''Faça um algoritmo que apresente todos os valores numéricos divisíveis por 4 e menores que 200'''
+for contador in range(0, 201):
+    if contador % 4 == 0:
+        print(contador, end=', ')
+print('FIM')
