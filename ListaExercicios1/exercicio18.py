6130eeddac45f6b7e4b29004a7629e522d6eb6ff
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 19:56:48 -0300

    Create exercicio18.py

---
+++ ListaExercicios1/exercicio18.py
@@ -0,0 +1,6 @@
+'''Elabore um programa que calcule o alcance de um projétil, dada a velocidade (Vo), o ângulo (ang) 
+entre o cano do canhão e o solo. A fórmula a ser utilizada é: S = (Vo Vo /g) sen(2ang), onde S é o alcance, 
+g é a gravidade, ang é dado em graus, sendo necessário converter para radianos.'''
+
+velocidade = float(input('Informe a velocidade do projetil: '))
+angulo = float(input('Informe o angulo: '))
