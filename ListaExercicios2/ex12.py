dcf47b8fc178883f615e62e52724cb0aea69b598
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 20:29:57 -0300

    Create ex12.py

---
+++ ListaExercicios2/ex12.py
@@ -0,0 +1,7 @@
+'''Faça um programa que determine se um dado número é par ou ímpar'''
+
+num = int(input('Informe um numero: '))
+if num % 2 == 0:
+    print('Numero par')
+else:
+    print('Numero impar')
