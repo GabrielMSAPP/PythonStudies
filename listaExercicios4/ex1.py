70167a76db1d9e8687e57648700c41be0a6873cc
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 22:15:32 -0300

    Create ex1.py

---
+++ listaExercicios4/ex1.py
@@ -0,0 +1,13 @@
+'''Elabore um programa que efetue a leitura de duas strings e informe o seu conteúdo, seguido de seu 
+comprimento. Indique também se as duas strings possuem o mesmo comprimento e se são iguais ou diferentes no
+conteúdo'''
+
+dicionario = {}
+for i in range(2):
+    palavra = input('Digite uma palavra: ')
+    dicionario[i] = [palavra, len(palavra)]
+    print(dicionario)
+if dicionario[0][0] == dicionario[1][0]:
+    print('Conteúdo iguais')
+if dicionario[0][1] == dicionario[1][1]:
+    print('Comprimento iguais')
