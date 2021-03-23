b6804a762fdc7f76b0bddda5f6207a6dadbe3573
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 22:15:36 -0300

    Create ex2.py

---
+++ listaExercicios4/ex2.py
@@ -0,0 +1,4 @@
+'''Elabore um programa que solicite ao usuário, o seu nome e em seguida mostre o seu 
+nome de trás para frente utilizando somente letras maiúsculas'''
+nome = input('Digite seu nome: ')
+print(nome[::-1].upper())
