04482feae11343029299e1dad471e64b9516f376
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-22 22:15:44 -0300

    Create ex5.py

---
+++ listaExercicios4/ex5.py
@@ -0,0 +1,12 @@
+'''Faça um programa que leia um número de telefone, e corrija o número no caso deste conter 
+somente 7 dígitos, acrescentando o ’3’ na frente. O usuário pode informar o número com ou 
+sem o traço separador'''
+
+telefone = input('Digite um telefone: ')
+traco = False
+for i in telefone:
+    if i == '-':
+        traco = True
+if len(telefone) == 7 or len(telefone) == 8 and traco:
+    telefone = '3' + telefone
+print(f'Seu telefone é: {telefone}')
